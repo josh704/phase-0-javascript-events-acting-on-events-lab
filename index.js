e947982355a93034ca1e7a4f3f5308f2ecb3da85
josh704 <joshuangetich02@gmail.com>
2024-10-07 12:55:12 +0300

    events-lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    const rightEdge = left + dodger.offsetWidth; 
+    const containerWidth = dodger.parentElement.offsetWidth; 
+
+    if (rightEdge < containerWidth) {
+        dodger.style.left = `${left + 1}px`; 
+    }
+}
+document.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (event.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
